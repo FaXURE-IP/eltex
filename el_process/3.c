02dbe4d040c4cf38d2ab26718785c4ad76bb27c9
FaXURE-IP <124141246+FaXURE-IP@users.noreply.github.com>
2024-09-15 11:21:26 +0700

    finish 12_lab

--- el_process/3.c
+++ el_process/3.c
@@ -7,12 +7,33 @@
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
+#define MAX_CMDS 16
+
+void execute_command(char *cmd)
+{
+    char *args[MAX_ARGS];
+    char *token = strtok(cmd, " ");
+    int i = 0;
+
+    while (token != NULL && i < MAX_ARGS - 1)
+    {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    if (execvp(args[0], args) == -1)
+    {
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main()
 {
     char input[MAX_INPUT];
-    char *args[MAX_ARGS];
-    char *token;
+    char *cmds[MAX_CMDS];
     int status;
     pid_t pid;
 
@@ -27,34 +48,75 @@ int main()
         {
             break;
         }
-        int i = 0;
-        token = strtok(input, " ");
-        while (token != NULL && i < MAX_ARGS - 1)
-        {
-            args[i] = token;
-            i++;
-            token = strtok(NULL, " ");
-        }
-        args[i] = NULL;
 
-        pid = fork();
-        if (pid < 0)
+        int num_cmds = 0;
+        char *token = strtok(input, "|");
+        while (token != NULL && num_cmds < MAX_CMDS - 1)
         {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
+            cmds[num_cmds] = token;
+            num_cmds++;
+            token = strtok(NULL, "|");
         }
+        cmds[num_cmds] = NULL;
 
-        if (pid == 0)
+        int pipefds[2 * (num_cmds - 1)];
+
+        for (int i = 0; i < num_cmds - 1; i++)
         {
-            if (execvp(args[0], args) == -1)
+            if (pipe(pipefds + i * 2) < 0)
             {
-                perror("execvp failed");
+                perror("pipe failed");
+                exit(EXIT_FAILURE);
             }
-            exit(EXIT_FAILURE);
         }
-        else
+
+        for (int i = 0; i < num_cmds; i++)
         {
-            waitpid(pid, &status, 0);
+            pid = fork();
+            if (pid < 0)
+            {
+                perror("fork failed");
+                exit(EXIT_FAILURE);
+            }
+
+            if (pid == 0)
+            {
+
+                if (i > 0)
+                {
+                    if (dup2(pipefds[(i - 1) * 2], STDIN_FILENO) < 0)
+                    {
+                        perror("dup2 failed (input)");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+
+                if (i < num_cmds - 1)
+                {
+                    if (dup2(pipefds[i * 2 + 1], STDOUT_FILENO) < 0)
+                    {
+                        perror("dup2 failed (output)");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+                for (int j = 0; j < 2 * (num_cmds - 1); j++)
+                {
+                    close(pipefds[j]);
+                }
+
+                execute_command(cmds[i]);
+            }
+        }
+
+        for (int i = 0; i < 2 * (num_cmds - 1); i++)
+        {
+            close(pipefds[i]);
+        }
+        for (int i = 0; i < num_cmds; i++)
+        {
+            wait(&status);
         }
     }
 
